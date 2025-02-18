595d63751af254c5fda3c0be0d1b7effc512d8a1
SverreNystad <sverrenystad@gmail.com>
2025-02-18 15:25:43 +0100

    feat: add interfaces for selection, expansion, simulation, and backpropagation in mcts

--- src/search/mcts.py
+++ src/search/mcts.py
@@ -1,6 +1,14 @@
 from dataclasses import dataclass
-
+import time
 import torch
+
+from src.search.nodes import Node
+from src.search.strategies import (
+    SelectionStrategy,
+    ExpansionStrategy,
+    SimulationStrategy,
+    BackpropagationStrategy,
+)
 
 
 @dataclass
@@ -11,3 +19,44 @@ class MCTSOutput:
 
     action_probs: torch.Tensor
     value: torch.float
+
+
+class MCTS:
+
+    def __init__(
+        self,
+        selection: SelectionStrategy,
+        expansion: ExpansionStrategy,
+        simulation: SimulationStrategy,
+        backpropagation: BackpropagationStrategy,
+        max_itr: int = 0,
+        max_time: float = 0.0,
+    ) -> None:
+        self.selection = selection
+        self.expansion = expansion
+        self.simulation = simulation
+        self.backpropagation = backpropagation
+        self.max_itr = max_itr
+        self.max_time = max_time
+
+    def run(self, root: Node) -> MCTSOutput:
+        """
+        Run the Monte Carlo Tree Search algorithm mutating the tree starting at `root`.
+        """
+        if self.max_itr == 0:
+            start_time = time.time()
+            while time.time() - start_time < self.max_time:
+                chosen_node = self.selection(root)
+                created_node = self.expansion(chosen_node)
+                result = self.simulation(created_node)
+                self.backpropagation(created_node, result)
+        else:
+            itr = 0
+            while itr < self.max_itr:
+                chosen_node = self.selection(root)
+                created_node = self.expansion(chosen_node)
+                result = self.simulation(created_node)
+                self.backpropagation(created_node, result)
+                itr += 1
+
+        # TODO: Calculate the action probabilities and the value of the root node
---
+++ src/search/strategies.py
@@ -0,0 +1,54 @@
+"""
+This module contains the interfaces for the different strategies used in the MCTS algorithm.
+To read more about the strategy design pattern, see https://refactoring.guru/design-patterns/strategy
+"""
+
+from typing import Protocol
+from src.search.nodes import Node
+
+
+class SelectionStrategy(Protocol):
+    def __call__(self, root: Node) -> Node:
+        """
+        Select a node from the tree starting at `root` for further expansion.
+
+        Args:
+            - root (Node) The root node of the tree.
+        Returns:
+            The selected node.
+        """
+        ...
+
+
+class ExpansionStrategy(Protocol):
+    def __call__(self, node: Node) -> Node:
+        """
+        Expand the given node by adding one or more child nodes and return one of them.
+        """
+        ...
+
+
+class SimulationStrategy(Protocol):
+    def __call__(self, node: Node) -> float:
+        """
+        Simulate a play-out (rollout) from the given node to the end of the game.
+
+        Args:
+            - node (Node) The node from which to start the simulation.
+        Returns:
+            The result of the simulation
+        """
+        ...
+
+
+class BackpropagationStrategy(Protocol):
+    def __call__(self, node: Node, result: float) -> None:
+        """
+        Backpropagate the simulation result up through the tree.
+
+        Args:
+            - node (Node) The node from which the simulation started.
+            - result (float) The result of the simulation
+        """
+        # TODO: Find out how to determine the flipping of the result for adversarial games and non-adversarial games and for team games
+        ...
