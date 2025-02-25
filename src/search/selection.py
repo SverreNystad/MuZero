c8a18b8c09aeb28aaae2421cbb33620b61444607
SverreNystad <sverrenystad@gmail.com>
2025-02-25 01:33:56 +0100

    feat: add UCT and PUCT selection strategies for search module

---
+++ src/search/selection.py
@@ -0,0 +1,60 @@
+import math
+from src.search.nodes import Node
+from src.search.strategies import SelectionStrategy
+
+
+class UCT(SelectionStrategy):
+    """
+    Upper Confidence bounds applied to Trees
+    """
+
+    def __init__(self, c: float = 1.41):
+        self.c = c
+
+    def uct_score(self, node: Node) -> float:
+        prior_score = node.reward / node.visit_count
+        value_score = (
+            self.c
+            * math.sqrt(math.log(node.parent.visit_count))
+            / (node.visit_count + 1)
+        )
+        return prior_score + value_score
+
+    def __call__(self, root: Node) -> Node:
+        while len(root.children.values()) > 0:
+            root = max(root.children.values(), key=self.uct_score)
+        return root
+
+
+class PUCT(SelectionStrategy):
+    """
+    Predictor Upper Confidence Bound applied to Trees
+    """
+
+    def __init__(self, c1: float = 1.25, c2: float = 19652, discount: float = 0.99):
+        """
+        Args:
+            - c1 (float): The PUCT-initializer parameter set as 1.25 as (Silver et al., 2017) suggests.
+            - c2 (float): The PUCT-base parameter set as 19652 as (Silver et al., 2017) suggests.
+        """
+        self.c1 = c1
+        self.c2 = c2
+        self.discount = discount
+
+    def uct_score(self, node: Node) -> float:
+        pb_c = math.sqrt(node.parent.visit_count) / (node.visit_count + 1)
+        pb_c *= math.log((node.visit_count + self.c2 + 1) / self.c2) + self.c1
+
+        prior_score = pb_c * node.parent.value_sum
+        if node.visit_count > 0:
+            value_score = (
+                node.reward + self.discount * node.value_sum / node.visit_count
+            )
+        else:
+            value_score = 0
+        return prior_score + value_score
+
+    def __call__(self, root: Node) -> Node:
+        while len(root.children.values()) > 0:
+            root = max(root.children.values(), key=self.uct_score)
+        return root
