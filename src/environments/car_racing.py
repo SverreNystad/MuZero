6fd4157533075a8416af0ab005179982e16efd4d
Sverre Nystad <sverrenystad@gmail.com>
2025-03-03 17:35:24 +0100

    Merge branch 'main' of github.com:SverreNystad/MuZero

--- src/environments/car_racing.py
+++ src/environments/car_racing.py
@@ -1,7 +1,40 @@
-import gym
 
-env = gym.make("CarRacing-v0")
-env.reset(seed=42)
+from gym.envs.box2d.car_racing import CarRacing as CarRacingGym
+from gym.spaces.discrete import Discrete
 
-env.render()
+from pydantic import BaseModel
+from src.environment import Environment
 
+class CarRacingConfig(BaseModel):
+    seed: int
+
+class CarRacing(Environment):
+    def __init__(self, config: CarRacingConfig):
+        self.env = CarRacingGym(render_mode="rgb_array", continuous=False)
+        self.env.reset(seed=config.seed)
+        self.observation_space = self.env.observation_space
+
+    def get_action_space(self) -> tuple:
+        space: Discrete = self.env.action_space
+        return space.start, space.n
+
+    def get_observation_space(self) -> tuple:
+        return self.observation_space.shape
+
+    def step(self, action: int) -> tuple:
+        state = self.env.step(action)
+        observation, reward, termination, truncated, info = state
+        return observation, reward, termination
+
+    def get_state(self) -> any:
+        return self.env.last()[0]
+
+    def reset(self) -> any:
+        return self.env.reset()
+
+    def render(self) -> any:
+        return self.env.render()
+    
+    def close(self) -> any:
+        return self.env.close()
+    
--- tests/search/test_expansion.py
+++ tests/search/test_expansion.py
@@ -2,14 +2,24 @@ from src.search.expansion import expand_node
 from src.neural_network import DynamicsNetwork
 from src.search.nodes import Node
 import torch
+import pytest
 
 
-def test_expanding_leaf_node():
-    batch_size = 2
-    latent_state = torch.randn(batch_size, 10)
+@pytest.mark.parametrize(
+    "batch_size" "action_space" "latent_dim",
+    [
+        (2, 2, 10),
+        (3, 3, 5),
+    ],
+)
+def test_expanding_leaf_node(batch_size, action_space, latent_dim):
+    latent_state = torch.randn(batch_size, latent_dim)
     root = Node(latent_state)
-    dynamics_network = DynamicsNetwork(10, 2)
+    dynamics_network = DynamicsNetwork(latent_dim, action_space)
     assert len(root.children) == 0
-    possible_actions = [torch.tensor([1] * batch_size), torch.tensor([0] * batch_size)]
+    possible_actions = []
+    for i in range(action_space):
+        possible_actions.append(torch.tensor([i] * batch_size))
+
     expand_node(root, possible_actions, dynamics_network)
     assert len(root.children) == len(possible_actions)
