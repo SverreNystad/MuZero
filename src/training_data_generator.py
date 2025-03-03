b8e313cc5700c5ecc2db889528ff638eb55e759c
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-03 16:58:28 +0100

    feat: Stop episode when done

--- src/training_data_generator.py
+++ src/training_data_generator.py
@@ -4,23 +4,28 @@ from dataclasses import dataclass
 from src.environment import Environment
 from src.neural_network import RepresentationNetwork, PredictionNetwork, DynamicsNetwork
 
+
 @dataclass
-class GameState:
+class Chunk:
     """
-    Data class to store game state data.
+    Data class to store chunk state data.
     """
-    state: Environment
+
+    state: Tensor
     policy: Tensor
     reward: float
-    value: float # TODO: Could be a Tensor with shape (1, 1)
-    best_action: Tensor # TODO: Could be an int
+    value: float  # TODO: Could be a Tensor with shape (1, 1)
+    best_action: Tensor  # TODO: Could be an int
+
 
 @dataclass
 class Episode:
     """
     Data class to store episode data.
     """
-    states: list[GameState]
+
+    chunks: list[Chunk]
+
 
 class TrainingDataGenerator:
     def __init__(
@@ -37,9 +42,9 @@ class TrainingDataGenerator:
         self.repr_net = repr_net
         self.dyn_net = dyn_net
         self.pred_net = pred_net
-        self.num_episodes: int = config["num_episodes"] # N_e
-        self.max_steps: int = config["max_steps"] # N_es
-        self.look_back: int = config["look_back"] # q
+        self.num_episodes: int = config["num_episodes"]  # N_e
+        self.max_steps: int = config["max_steps"]  # N_es
+        self.look_back: int = config["look_back"]  # q
         self.total_time: float = config["total_time"]
 
     def generate_training_data(self) -> list[Episode]:
@@ -49,29 +54,44 @@ class TrainingDataGenerator:
             total_time: Total time to run the episodes in milliseconds (ms).
 
         Returns:
-            List of episode data. 
+            List of episode data.
         """
-        episode_history = []
+        episode_history: list[Episode] = []
 
         for _ in trange(self.num_episodes):
             self.env.reset()
 
-            epidata = Episode(states=[])
-
+            epidata = Episode(chunks=[])
+            state = self.env.get_state()
             for _ in trange(self.max_steps):
-                latent_state = self.repr_net(self.env.state)
+                latent_state = self.repr_net(state)
 
                 tree_policy: Tensor
-                tree_policy, value = self.mcts(latent_state=latent_state, critic_model=self.pred_net, dynamics_model=self.dyn_net)
+                tree_policy, value = self.mcts(
+                    latent_state=latent_state,
+                    critic_model=self.pred_net,
+                    dynamics_model=self.dyn_net,
+                )
 
                 # TODO: Sample action from tree policy
                 action = tree_policy[0]
-                
-                self.env.step(action)
-                epidata.states.append(self.env.state)
+
+                next_state, next_reward, done = self.env.step(action)
+                chunk = Chunk(
+                    state=state,
+                    policy=tree_policy,
+                    reward=next_reward,
+                    value=value,
+                    best_action=action,
+                )
+                epidata.chunks.append(chunk)
+                state = next_state
+
+                # TODO: Backpropagate actual reward found in terminal state
+
+                if done:
+                    break
 
             episode_history.append(epidata)
-
-            # TODO: Backpropagate actuale reward found in terminal state
 
         return episode_history
