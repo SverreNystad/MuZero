1785458dfa499eb7ffbc2401630394be20027581
Sverre Nystad <sverrenystad@gmail.com>
2025-03-04 12:36:57 +0100

    feat: update create_environment function to specify config types for CarRacing and ConnectFour and add CarRacing env

--- src/environments/factory.py
+++ src/environments/factory.py
@@ -1,14 +1,16 @@
-from typing import Any, Union
+from typing import Union
 from src.environment import Environment
+from src.environments.car_racing import CarRacing, CarRacingConfig
 from src.environments.connect_four import ConnectFour, ConnectFourConfig
 
 
-def create_environment(env_config: Union[Any]) -> Environment:
+def create_environment(
+    env_config: Union[CarRacingConfig, ConnectFourConfig]
+) -> Environment:
 
     match env_config:
-        # TODO: add more cases here
-        # case SpecificEnvironmentConfig:
-        #   return SpecificEnvironment(env_config)
+        case CarRacingConfig():
+            return CarRacing(env_config)
         case ConnectFourConfig():
             return ConnectFour(env_config)
         case _:
