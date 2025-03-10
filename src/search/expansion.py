c69e174e70e6d71c3bceeb0998a7f4164b61bd6e
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-10 18:23:06 +0100

    refactor: Introduce batch inference of expansion
    Co-authored-by: Sverre Nystad <sverrenystad@gmail.com>

--- src/search/expansion.py
+++ src/search/expansion.py
@@ -1,23 +1,35 @@
 from src.neural_network import DynamicsNetwork
 from src.search.nodes import Node
-from torch import Tensor
-import random
+import random, torch
 
 
 def expand_node(
-    node: Node, possible_actions: list[Tensor], dynamics_network: DynamicsNetwork
+    node: Node,
+    actions: torch.Tensor,
+    dynamics_network: DynamicsNetwork,
 ) -> Node:
     """
-    Expand the given node by adding one or more child nodes and return one of them
+    Expand the given node by adding all child nodes (based on actions) and return one of them
 
     Args:
         - node (Node): The node to expand from
     """
-    if len(possible_actions) == 0:
+    batch_size = actions.size(0)
+    if batch_size <= 0:
         raise ValueError("No possible actions to expand the node with")
 
-    for action in possible_actions:
-        next_latent_state, reward = dynamics_network(node.latent_state, action)
-        child = node.add_child(next_latent_state, action)
-        child.reward = reward
+    transformed_latent_state = _transform_latent_state(batch_size, node.latent_state)
+
+    next_latent_states, rewards = dynamics_network(transformed_latent_state, actions)
+    for i in range(batch_size):
+        child = node.add_child(next_latent_states[i], actions[i])
+        child.reward = rewards[i]
+
     return random.choice(list(node.children.values()))
+
+
+def _transform_latent_state(
+    batch_size: int, latent_state: torch.Tensor
+) -> torch.Tensor:
+    """Transforms the latent state into the correct shape for the batch size"""
+    return latent_state.repeat(batch_size, 1)
