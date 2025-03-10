d18b64c7dc0e6124ff2c48387a718ecd555e408f
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-10 18:23:54 +0100

    docs: Add test intent

--- tests/search/test_simulation.py
+++ tests/search/test_simulation.py
@@ -5,8 +5,13 @@ import torch
 
 
 def test_muzero_simulation():
+    """No actual rollouts, only predictions from current latent state"""
     predictor = PredictionNetwork(latent_dim=10, num_actions=2)
     simulation = MuZeroSimulation(predictor)
     node = Node(torch.randn(1, 10))
     value = simulation(node)
     assert isinstance(value, float)
+
+
+def test_alphazero_simulation():
+    """Rollouts with real moves to terminal and return winner"""
