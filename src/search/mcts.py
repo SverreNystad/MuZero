175e26862ecbb2743ac633ab7c42f8115bf8b7de
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-03-10 19:33:24 +0100

    refactor: Change MCTS to follow the new signature and refactor step function
    Co-authored-by: Sverre Nystad <sverrenystad@gmail.com>

--- src/search/mcts.py
+++ src/search/mcts.py
@@ -1,24 +1,14 @@
-from dataclasses import dataclass
 import time
 import torch
 
+from src.neural_network import DynamicsNetwork
 from src.search.nodes import Node
 from src.search.strategies import (
     SelectionStrategy,
-    ExpansionStrategy,
     SimulationStrategy,
     BackpropagationStrategy,
 )
-
-
-@dataclass
-class MCTSOutput:
-    """
-    Output of the MCTS algorithm.
-    """
-
-    action_probs: torch.Tensor
-    value: torch.float
+from src.search.expansion import expand_node
 
 
 class MCTS:
@@ -26,20 +16,22 @@ class MCTS:
     def __init__(
         self,
         selection: SelectionStrategy,
-        expansion: ExpansionStrategy,
         simulation: SimulationStrategy,
         backpropagation: BackpropagationStrategy,
+        dynamic_network: DynamicsNetwork,
+        actions: torch.Tensor,
         max_itr: int = 0,
         max_time: float = 0.0,
     ) -> None:
         self.selection = selection
-        self.expansion = expansion
         self.simulation = simulation
         self.backpropagation = backpropagation
+        self.actions = actions
+        self.dynamics_network = dynamic_network
         self.max_itr = max_itr
         self.max_time = max_time
 
-    def run(self, root: Node) -> MCTSOutput:
+    def run(self, root: Node) -> None:
         """
         Run the Monte Carlo Tree Search algorithm mutating the tree starting at `root`.
         """
@@ -47,16 +39,24 @@ class MCTS:
             start_time = time.time()
             while time.time() - start_time < self.max_time:
                 chosen_node = self.selection(root)
-                created_node = self.expansion(chosen_node)
+                created_node = expand_node(
+                    chosen_node, self.actions, self.dynamics_network
+                )
                 result = self.simulation(created_node)
-                self.backpropagation(created_node, result)
+                self.backpropagation(created_node, result, root.to_play)
         else:
             itr = 0
             while itr < self.max_itr:
-                chosen_node = self.selection(root)
-                created_node = self.expansion(chosen_node)
-                result = self.simulation(created_node)
-                self.backpropagation(created_node, result)
+                _step(root)
                 itr += 1
 
         # TODO: Calculate the action probabilities and the value of the root node
+
+    def _step(self, node: Node) -> None:
+        """
+        Run a single step of the Monte Carlo Tree Search algorithm.
+        """
+        chosen_node = self.selection(node)
+        created_node = expand_node(chosen_node, self.actions, self.dynamics_network)
+        result = self.simulation(created_node)
+        self.backpropagation(created_node, result, chosen_node.to_play)
