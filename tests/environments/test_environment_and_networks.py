36a820d5866d8733f9f706e61d2cdc011365d1ea
SverreNystad <sverrenystad@gmail.com>
2025-03-13 00:27:00 +0100

    refactor: Update test_environments_states_with_representation_network to work for several environments

--- tests/environments/test_environment_and_networks.py
+++ tests/environments/test_environment_and_networks.py
@@ -1,16 +1,25 @@
+import pytest
+
 from src.environment import Environment
 from src.environments.factory import create_environment
-from src.environments.car_racing import CarRacing, CarRacingConfig
+from src.environments.car_racing import CarRacingConfig
+from src.environments.connect_four import ConnectFourConfig
 from src.neural_network import RepresentationNetwork
 
 
-def test_car_racing_with_representation_network():
-    env: CarRacing = create_environment(CarRacingConfig())
+@pytest.mark.parametrize(
+    "env_config",
+    [
+        CarRacingConfig(seed=42, render_mode="rgb_array"),  # (3, 96, 96)
+    ],
+)
+def test_environments_states_with_representation_network(env_config: CarRacingConfig):
+    env: Environment = create_environment(env_config)
     latent_dim = 32
     channels, height, width = env.get_observation_space()
     repr_net = RepresentationNetwork(channels, (height, width), latent_dim)
 
     # Test the representation network with a random observation
-    obs = env.reset()  # (3, 96, 96)
+    obs = env.reset()
     latent = repr_net(obs)
     assert latent.shape == (1, latent_dim)
