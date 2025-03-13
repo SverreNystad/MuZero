c5106281e5e590bd43b6df223733f79ad3f8cd98
SverreNystad <sverrenystad@gmail.com>
2025-03-13 01:37:49 +0100

    refactor: Remove unused files

--- src/configuration.py
+++
@@ -1,5 +0,0 @@
-# Loss function parameters
-c_r = 1.0
-c_v = 1.0
-c_p = 1.0
-
--- test.py
+++
@@ -1,6 +0,0 @@
-from src.environments.connect_four import ConnectFour, ConnectFourConfig
-from src.environments.car_racing import CarRacing, CarRacingConfig
-
-env = CarRacing(CarRacingConfig())
-print(env.get_observation_space())
-print(env.get_action_space())
