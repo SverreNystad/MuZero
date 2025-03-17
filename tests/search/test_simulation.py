3f679a08d4d169281be9641a49fe2416d683ff2c
SverreNystad <sverrenystad@gmail.com>
2025-03-18 00:11:03 +0100

    fix: correct instantiation of networks in some tests

--- tests/search/test_simulation.py
+++ tests/search/test_simulation.py
@@ -3,16 +3,19 @@ from src.search.nodes import Node
 from src.search.simulation import MuZeroSimulation
 import torch
 
+from tests.nerual_networks.test_networks import tiny_dyn_net, tiny_pred_net
+
 
 def test_muzero_simulation():
     """No actual rollouts, only predictions from current latent state"""
     num_actions = 2
-    latent_dim = 10
     depth = 5
-    predictor = PredictionNetwork(latent_dim=latent_dim, num_actions=num_actions)
-    dynamic_network = DynamicsNetwork(latent_dim=latent_dim, num_actions=num_actions)
-    simulation = MuZeroSimulation(dynamic_network, predictor, depth=depth)
-    node = Node(torch.randn(latent_dim))
+    latent_shape = (2, 2, 2)
+    dyn_net = tiny_dyn_net(latent_shape, num_actions=num_actions)
+    pred_net = tiny_pred_net(latent_shape, num_actions=num_actions)
+
+    simulation = MuZeroSimulation(dyn_net, pred_net, depth)
+    node = Node(torch.randn(*latent_shape))
     rewards = simulation(node)
     assert isinstance(rewards, list)
     assert len(rewards) == depth + 1
