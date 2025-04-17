91bae89d15d758eee8fd6080d5c649b0342a1634
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-17 19:53:45 +0200

    feat: Add Lunar Lander environment to the environment factory

--- src/environments/factory.py
+++ src/environments/factory.py
@@ -3,10 +3,13 @@ from torch._prims_common import DeviceLikeType
 from src.environment import Environment
 from src.environments.car_racing import CarRacing, CarRacingConfig
 from src.environments.connect_four import ConnectFour, ConnectFourConfig
+from src.environments.lunar_lander import LunarLander, LunarLanderConfig
 
 
-def create_environment(env_config: CarRacingConfig | ConnectFourConfig, device: DeviceLikeType = "cpu") -> Environment:
+def create_environment(env_config: CarRacingConfig | ConnectFourConfig | LunarLanderConfig, device: DeviceLikeType = "cpu") -> Environment:
     match env_config:
+        case LunarLanderConfig():
+            return LunarLander(env_config, device)
         case CarRacingConfig():
             return CarRacing(env_config, device)
         case ConnectFourConfig():
