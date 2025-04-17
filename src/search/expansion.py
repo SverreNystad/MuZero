0aea4c6781a57002771b1674ba5b8f2a0db95331
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-17 22:26:21 +0200

    feat: Add prediction network to expansion to support policy priority

--- src/search/expansion.py
+++ src/search/expansion.py
@@ -2,7 +2,7 @@ import random
 
 import torch
 
-from src.neural_networks.neural_network import DynamicsNetwork
+from src.neural_networks.neural_network import DynamicsNetwork, PredictionNetwork
 from src.search.nodes import Node
 
 
@@ -10,6 +10,7 @@ def expand_node(
     node: Node,
     actions: torch.Tensor,
     dynamics_network: DynamicsNetwork,
+    prediction_network: PredictionNetwork,
 ) -> Node:
     """
     Expand the given node by adding all child nodes (based on actions) and return one of them
@@ -22,10 +23,13 @@ def expand_node(
         raise ValueError("No possible actions to expand the node with")
 
     transformed_latent_state = _transform_latent_state(batch_size, node.latent_state)
+    policy, _ = prediction_network(node.latent_state)
+    policy = torch.softmax(policy, dim=1).squeeze(0)
 
     next_latent_states, rewards = dynamics_network(transformed_latent_state, actions)
     for i in range(batch_size):
-        child = node.add_child(next_latent_states[i], actions[i])
+        child = node.add_child(next_latent_states[i].unsqueeze(0), actions[i]) # (4, 6, 6, 3) -> [4 * [...]], (6, 6, 3) -> unsqueeze -> (1, 6, 6, 3)
+        child.policy_priority = policy[i].item() #shape (1, 4) -> [[1, 1, 1, 1]]
         child.reward = rewards[i]
 
     return random.choice(list(node.children.values()))
