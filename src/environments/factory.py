908172d52c87e5957c0310a7855c0d20bade1fbd
Simon Sandvik Lee <simonsandviklee@gmail.com>
2025-04-18 02:39:04 +0200

    feat: Add Flappy bird environment

--- src/environments/factory.py
+++ src/environments/factory.py
@@ -3,10 +3,11 @@ from torch._prims_common import DeviceLikeType
 from src.environment import Environment
 from src.environments.car_racing import CarRacing, CarRacingConfig
 from src.environments.connect_four import ConnectFour, ConnectFourConfig
+from src.environments.flappy_bird import FlappyBird, FlappyBirdConfig
 from src.environments.lunar_lander import LunarLander, LunarLanderConfig
 
 
-def create_environment(env_config: CarRacingConfig | ConnectFourConfig | LunarLanderConfig, device: DeviceLikeType = "cpu") -> Environment:
+def create_environment(env_config: CarRacingConfig | ConnectFourConfig | LunarLanderConfig | FlappyBirdConfig, device: DeviceLikeType = "cpu") -> Environment:
     match env_config:
         case LunarLanderConfig():
             return LunarLander(env_config, device)
@@ -14,5 +15,7 @@ def create_environment(env_config: CarRacingConfig | ConnectFourConfig | LunarLa
             return CarRacing(env_config, device)
         case ConnectFourConfig():
             return ConnectFour(env_config, device)
+        case FlappyBirdConfig():
+            return FlappyBird(env_config, device)
         case _:
             raise ValueError("Invalid environment configuration")
