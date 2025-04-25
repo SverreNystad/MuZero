f07209ca2da8684caf12d893358282db7ca22ae8
Kristoffer Olaisen <knolaisen@gmail.com>
2025-04-25 11:42:52 +0200

    -feat: add reward for staying in pipe gap area

--- src/environments/flappy_bird.py
+++ src/environments/flappy_bird.py
@@ -2,6 +2,10 @@ from typing import Any, Literal
 
 import torch
 from flappy_bird_gymnasium.envs.flappy_bird_env import FlappyBirdEnv
+from flappy_bird_gymnasium.envs.constants import (
+    PIPE_HEIGHT,
+)
+
 from gym.spaces.discrete import Discrete
 from pydantic import BaseModel
 from torch import Tensor
@@ -44,13 +48,39 @@ class FlappyBird(Environment):
         """
         return (3, 512, 288)
 
+    # def step(self, action: int) -> tuple[Tensor, float, bool]:
+    #     step = self.env.step(action)
+    #     _, reward, done, _ = step[0], step[1], step[2], step[3]
+    #     obs_t = torch.from_numpy(self.render()).float().permute(2, 0, 1).to(self.device) # (3, 512, 288)
+    #     obs_t = obs_t.unsqueeze(0) # (1, 3, 512, 288)
+    #     self.last_obs = obs_t
+    #     
+    #     return obs_t, reward, done
     def step(self, action: int) -> tuple[Tensor, float, bool]:
         step = self.env.step(action)
         _, reward, done, _ = step[0], step[1], step[2], step[3]
         obs_t = torch.from_numpy(self.render()).float().permute(2, 0, 1).to(self.device) # (3, 512, 288)
         obs_t = obs_t.unsqueeze(0) # (1, 3, 512, 288)
         self.last_obs = obs_t
+        
+        #find first unpassed pipe
+        player_y = self.env._player_y
+        player_x = self.env._player_x
+        min_distance = 1000
+        closest_pipe = None
+        for i, pipe in enumerate(self.env._upper_pipes):
+            pipe_x = pipe["x"]
+            distance = pipe_x - player_x
+            if distance > 0 and distance < min_distance:
+                min_distance = distance
+                closest_pipe = i
+        
+        gap_top = self.env._upper_pipes[closest_pipe]["y"] + PIPE_HEIGHT
+        gap_bottom = self.env._lower_pipes[closest_pipe]["y"]
+        if gap_top < player_y < gap_bottom:
+            reward = 0.2
         return obs_t, reward, done
+        
 
     def get_state(self) -> Tensor:
         """
@@ -64,6 +94,7 @@ class FlappyBird(Environment):
         obs_t = obs_t.unsqueeze(0)  # (1, 3, 512, 288)
         self.last_obs = obs_t
         return obs_t
+
 
     def render(self) -> Any:
         return self.env.render()
